e14e93be8d62ad0e8834d80652e8cfda8f6f50ad
JackAlpaca <blood888123@gmail.com>
2022-09-03 13:09:16 +0800

    加入Boss相機與程式

---
+++ Assets/04.Scripts/BossCam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCam : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform target;
+
+    private void OnEnable()
+    {
+        FindPlayer();
+    }
+
+    void Update()
+    {
+        CamFollow();
+    }
+
+    public void FindPlayer()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void CamFollow()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -6.5f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/04.Scripts/CamManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamManager : MonoBehaviour
+{
+    public Camera MainCam;
+    public Camera BossCam;
+
+    public void CamSwitch()
+    {
+        if(MainCam.enabled)
+        {
+            MainCam.enabled = false;
+            BossCam.enabled = true;
+        }
+        else
+        {
+            BossCam.enabled = false;
+            MainCam.enabled = true;
+        }
+    }
+}
