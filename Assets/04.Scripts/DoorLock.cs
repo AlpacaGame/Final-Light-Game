dffb15a4921cf87632bb1d502ec1e39a8510e566
JackAlpaca <blood888123@gmail.com>
2022-09-06 13:22:33 +0800

    加入DoorLock程式/把Boss場景地板Collider改成一體成形
    用來偵測玩家靠近，然後播放開門動畫
    
    改Collider後，玩家走路才不會卡住

---
+++ Assets/04.Scripts/DoorLock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLock : MonoBehaviour
+{
+    public static bool DoorOpen = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            DoorOpen = true;
+        }
+    }
+}
