1a8efc914d176159b5b9e81029e789c8e9160766
PanYuMo <es649483@gmail.com>
2022-10-07 02:40:33 +0800

    10/7

---
+++ Assets/04.Scripts/關卡/DoorCollider2D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCollider2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D Fade)
+    {
+        if (Fade.CompareTag("Player"))
+        {
+            LevelControl1.間接開門 = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D Fade)
+    {
+        if (Fade.CompareTag("Player"))
+        {
+            LevelControl1.間接開門 = false;
+        }
+    }
+}
