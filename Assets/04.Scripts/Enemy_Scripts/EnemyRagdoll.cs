ded02fcaafe73b2fc432d50238764569b9a2e8a2
JackAlpaca <blood888123@gmail.com>
2022-11-13 15:40:53 +0800

    殭屍狗做布娃娃
    程式寫到一半發現2019不能用

---
+++ Assets/04.Scripts/Enemy_Scripts/EnemyRagdoll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.IK;
+
+public class EnemyRagdoll : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+    [SerializeField] private List<Collider2D> _colliders;
+    [SerializeField] private List<HingeJoint2D> _joints;
+    [SerializeField] private List<Rigidbody2D> _rbs;
+    [SerializeField] private List<LimbSolver2D> _solvers;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
