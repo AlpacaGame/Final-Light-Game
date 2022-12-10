43010ec3f77ecd235cfdb1694bf9d89371faaa8b
PanYuMo <es649483@gmail.com>
2022-12-10 23:06:20 +0800

    螢幕震動(未放入碰撞震動)

---
+++ Assets/測試東西的/ScreenShake.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    public static ScreenShake instance;
+
+    private float shakeTimeRemaiing, shakePower, shakeFadeTime, shakeRotation;
+
+    public float rotationMultiplier = 7.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            StartShake(.5f, 1f); //測試震動相機
+        }
+        */
+    }
+
+    private void LateUpdate()
+    {
+        if(shakeTimeRemaiing > 0)
+        {
+            shakeTimeRemaiing -= Time.deltaTime;
+
+            float xAmount = Random.Range(-1f, 1f) * shakePower;
+            float yAmount = Random.Range(-1f, 1f) * shakePower;
+
+            transform.position += new Vector3(xAmount, yAmount, 0f);
+
+            shakePower = Mathf.MoveTowards(shakePower, 0f, shakeFadeTime * Time.deltaTime);
+        
+            shakeRotation = Mathf.MoveTowards(shakeRotation, 0f, shakeFadeTime * rotationMultiplier * Time.deltaTime);
+        }
+
+        transform.rotation = Quaternion.Euler(0f, 0f, shakeRotation * Random.Range(-1f, 1f));
+    }
+
+    public void StartShake(float length, float power)
+    {
+        shakeTimeRemaiing = length;
+        shakePower = power;
+
+        shakeFadeTime = power / length;
+
+        shakeRotation = power * rotationMultiplier;
+    }
+}
