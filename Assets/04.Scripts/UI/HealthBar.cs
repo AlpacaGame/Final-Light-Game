8dbd8f8e64b761d66f042a784d7c5259a183f46a
JackAlpaca <blood888123@gmail.com>
2022-12-22 18:56:24 +0800

    血量UI完成

---
+++ Assets/04.Scripts/UI/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+	public Spore_Boos sporeBoss;
+	public Slider slider;
+
+	void Start()
+	{
+		slider.maxValue = sporeBoss.Hp;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		slider.value = sporeBoss.Hp;
+    }
+}
