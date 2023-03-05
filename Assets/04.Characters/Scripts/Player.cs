d237c77961746ac6b60354daef3b0e39df8c2240
JackAlpaca <blood888123@gmail.com>
2023-03-06 04:27:36 +0800

    玩家移動程式撰寫(未完成)

--- Assets/04.Characters/Scripts/Player.cs
+++ Assets/04.Characters/Scripts/Player.cs
@@ -5,6 +5,20 @@ using UnityEngine.U2D.IK;
 
 public class Player : MonoBehaviour
 {
+    [Header("玩家移動")]
+    public float energy = 100;//玩家體力
+    public float breatheEnergy = 50;//喘氣體力值
+    public float inputX = 0;//左右移動值
+    public float walkSpeed = 1.4f;//走路速度
+    public float runSpeed = 10;//跑步速度
+    public float runEnergyDecrease = 0.5f;//跑步體力消耗
+    public float walkEnergyIncrease = 0f;//走路體力增加
+    public float idleEnergyIncrease = 1f;//待機體力增加
+    public bool slide = false;
+    public bool breath = false;
+    public float playerSpeed = 0;
+    public float playerMaxSpeed = 5;
+
     [Header("生命值")]
     public int health = 100;
 
@@ -16,14 +30,98 @@ public class Player : MonoBehaviour
     [SerializeField] private List<Rigidbody2D> _rbs;
     [SerializeField] private List<LimbSolver2D> _solvers;
 
+    [Header("掛載元件")]
+    private Rigidbody2D rg;
+    private Animator anim;
+
     void Start()
     {
         ToggleRagdoll(false);//開始時關閉布娃娃系統
+
+        rg = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
 
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    //玩家移動
+    public void PlayerMove()
+    {
+        inputX = Input.GetAxis("Horizontal");
+
+        if(health > 0)
+        {
+            if (inputX != 0)
+            {
+                if (Input.GetKey(KeyCode.LeftShift) && energy > 50)//按下Shift鍵 & 體力不為0
+                {
+                    Run();
+                    anim.SetBool("isRun", true);
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && energy > 0)//按下Shift鍵 & 體力不為0
+                {
+                    Run();
+                    anim.SetBool("isFastWalk", true);
+                }
+                else
+                {
+                    Walk();
+                    anim.SetBool("isWalk", true);
+                    anim.SetBool("isFastWalk", false);
+                }
+            }
+            else
+            {
+                Idle();
+                anim.SetBool("isWalk", false);
+                anim.SetBool("isRun", false);
+            }
+        }
+    }
+
+    //待機
+    public void Idle()
+    {
+        //anim.Play("Idle");
+        energy += idleEnergyIncrease;//能量增加
+    }
+
+    //走路
+    public void Walk()
+    {
+        rg.velocity = new Vector2(inputX * walkSpeed, rg.velocity.y);
+        //anim.Play("Walk");
+        energy += walkEnergyIncrease;//能量增加
+    }
+
+    //跑步
+    public void Run()
+    {
+        rg.velocity = new Vector2(inputX * runSpeed, rg.velocity.y);
+        //anim.Play("Run");
+        energy -= runEnergyDecrease;//能量消耗
+
+        //喘氣聲
+        if (energy <= breatheEnergy && !breath)
+        {
+            breath = true;
+            SoundManager.instance.PantSource();
+            Invoke("BreatheTime", 5);
+        }
+    }
+
+    //喘氣聲間隔
+    public void BreatheTime()
+    {
+        breath = false;
     }
 
     //Ragdoll切換
