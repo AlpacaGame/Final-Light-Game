1fc5fd87a7a25aa28e6ef5a98ae83f7efdac78cb
JackAlpaca <blood888123@gmail.com>
2023-03-27 01:40:01 +0800

    拍攝用手臂旋轉完成

--- Assets/04.Scripts/Player/新Pivot.cs
+++ Assets/04.Scripts/Player/新Pivot.cs
@@ -11,9 +11,11 @@ public class 新Pivot : MonoBehaviour
 
     public Camera Orthographic;
 
+    public bool RecordMode = false;
+
     private void FixedUpdate()
     {
-        if (!玩家控制.切換使用敵人攝影機)
+        if(RecordMode)
         {
             Vector3 difference = Orthographic.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
@@ -31,6 +33,29 @@ public class 新Pivot : MonoBehaviour
             {
                 transform.localRotation = Quaternion.Euler(0, 0, rotationZ);//0, 0, rotationZ
                 玩家面相 = 1;
+            }
+        }
+        else
+        {
+            if (!玩家控制.切換使用敵人攝影機)
+            {
+                Vector3 difference = Orthographic.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+                difference.Normalize();
+
+                float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+                if (rotationZ < -90 || rotationZ > 90)
+                {
+                    transform.localRotation = Quaternion.Euler(180, 180, -rotationZ);//180, 180, -rotationZ
+                    玩家面相 = -1;
+                }
+
+                else if (rotationZ > -90 || rotationZ < 90)
+                {
+                    transform.localRotation = Quaternion.Euler(0, 0, rotationZ);//0, 0, rotationZ
+                    玩家面相 = 1;
+                }
             }
         }
     }
